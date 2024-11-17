8b7b5a866cc48f19379b5d2f0cba66e63326625c
Ivan <grenka388@gmail.com>
2024-11-17 19:19:01 +0300

    feat: [course3] [da] add lab7

---
+++ course3/da/lab7/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxRectangleWithZeros(const vector<vector<int>>& matrix, int n, int m) {
+	if (n == 0 || m == 0) return 0;
+	vector<int> height(m, 0);
+	int maxArea = 0;
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			height[j] = matrix[i][j] == 0 ? height[j] + 1: 0;
+		}
+
+		for (int j = 0; j < m; ++j) {
+			if (height[j] <= 0) {
+				continue;
+			}
+
+			int minHeight = height[j];
+			for (int k = j; k < m && height[k] > 0; ++k) {
+				minHeight = min(minHeight, height[k]);
+				int width = k - j + 1;
+				maxArea = max(maxArea, minHeight * width);
+			}
+		}
+	}
+
+	return maxArea;
+}
+
+int main() {
+    char ch;
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> matrix(n, vector<int>(m));
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cin >> ch;
+			matrix[i][j] = ch - '0';
+		}
+	}
+
+	cout << maxRectangleWithZeros(matrix, n, m) << endl;
+	return 0;
+}
